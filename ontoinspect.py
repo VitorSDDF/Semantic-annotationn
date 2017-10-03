adaf5b14e735d67e81f8b771e0d17cd500999cf3
VitorSDDF <vitor.sd@hotmail.com>
2017-10-03 00:06:50 -0300

    rascunho de anotação

--- ontoinspect.py
+++ ontoinspect.py
@@ -1,4 +1,6 @@
 from rdflib import Graph
+import datetime
+from rdflib.namespace import Namespace,RDFS,RDF
 import ontospy
 from rdflib import URIRef
 import nltk
@@ -7,10 +9,10 @@ from nltk.tag import pos_tag
 import re
 import urllib.request
 from bs4 import BeautifulSoup
-#from newspaper import Article
 from nltk.corpus import stopwords
 from bisect import bisect_left
 import urllib
+import datetime
  
 stop = stopwords.words('portuguese')
  
@@ -30,6 +32,32 @@ def ie_preprocess(document):
     #sentences = [nltk.pos_tag(sent) for sent in sentences]
     return sentences
 
+def annotation_to_rdf(creator,document,concept,authorRef,presentation):
+
+    AO = Namespace("http://smiy.sourceforge.net/ao/rdf/associationontology.owl")
+    PAV = Namespace("http://cdn.rawgit.com/pav-ontology/pav/2.0/pav.owl")
+    ANN = Namespace("https://www.w3.org/2000/10/annotation-ns#annotates")
+    AOF = Namespace("http://annotation-ontology.googlecode.com/svn/trunk/annotation-foaf.owl")
+
+    graph = Graph()
+    graph.bind('aof',AOF)
+    graph.bind('ao',AO)
+    graph.bind('ann',ANN)
+    graph.bind('pav',PAV)
+    lang = 'pt-br'
+    d = Describer(graph, base="www.descricao_anotacao.com")
+    d.value(RDFS.comment,presentation, lang=lang)
+    
+    d.rel(RDF.type,ANN.base)
+    d.rel(RDF.type,ANN.Annotation)
+    d.rel(RDF.type,AO.Annotation)
+    d.rel(AOF.annotatesDocument,document)
+    d.rel(AO.hasTopic,concept)
+    d.rel(PAV.createdOn,now = datetime.datetime.now())
+    d.rel(PAV.createdB,authorRef)
+
+    return graph
+    
 req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"}) 
 con = urllib.request.urlopen(req)
 soup = BeautifulSoup(con.read())
@@ -68,7 +96,7 @@ while len(reifications) > 0:
                     if concept not in a:
                         #Compara o conceito com os itens  de -(k + biggest_concept_lenght) até -k
                         if [i.upper() for i in concept] == [i.upper() for i in sentence[-(k + biggest_concept_lenght):-k]]:
-                            a.insert(len(a),concept)
+                            a.insert(len(a),concept) 
                             print('<' + url + ',' + "predicado" + ',' + str(concept) + '>')
                             biggest_concept_items.remove(concept)
 

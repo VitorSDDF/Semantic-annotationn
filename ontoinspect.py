51b236e8b988454cce5101081678aa4837942eb6
VitorSDDF <vitor.sd@hotmail.com>
2017-10-24 00:07:31 -0200

    Anotações em arquivo xml base

--- ontoinspect.py
+++ ontoinspect.py
@@ -1,25 +1,27 @@
-from rdflib import Graph,XSD,Literal
 import datetime
 import rdflib
+from rdflib import Graph,XSD,Literal,plugin,URIRef
 from rdflib.namespace import Namespace,RDFS,RDF,FOAF
 from rdflib.extras.describer import Describer
+from rdflib.serializer import Serializer
 import ontospy
-from rdflib import URIRef
 import nltk
-import rdflib
 from nltk.tag import pos_tag
-import re
 import urllib.request
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from bisect import bisect_left
 import urllib
-import datetime
+from urllib.parse import urlparse
+import shutil
+import os
+import tempfile
  
 stop = stopwords.words('portuguese')
  
 url = 'http://portalsaude.saude.gov.br/index.php/o-ministerio/principal/secretarias/svs/zika'
 text = "O zika é uma doença viral aguda, transmitida principalmente, pelos mosquitos Ae. Aegypti e Ae. albopictus, caracterizada por exantema maculopapular pruriginoso, febre intermitente, hiperemia conjuntival não purulenta e sem prurido, artralgia, mialgia e dor de cabeça. A maior parte dos casos apresentam evolução benigna e os sintomas geralmente desaparecem espontaneamente após 3-7 dias. No entanto, observa-se a ocorrência de óbitos pelo agravo, aumento dos casos de microcefalia e de manifestações neurológicas associadas à ocorrência da doença."
+text2 = "O vírus Zika é um flavivírus transmitido por mosquitos e foi, pela primeira vez, identificado em macacos, no Uganda, em 1947, através de uma rede que monitorizava a febre amarela. Foi mais tarde identificado em humanos, em 1952, no Uganda e na República Unida da Tanzânia. Foram registados surtos da doença do vírus Zika em África, nas Américas, na Ásia e no Pacífico. Entre os anos 1960 e os anos 1980, foram encontradas infecções humanas em África e na Ásia, normalmente acompanhadas de doença ligeira. O primeiro grande surto da doença causado pela infecção por Zika foi notificada na ilha de Yap (Estados Federados da Micronésia), em 2007. Em Julho de 2015, o Brasil notificou uma associação entre a infecção pelo vírus Zika e a síndrome de Guillain-Barré. Em Outubro de 2015, o Brasil notificou uma associação entre a infecção pelo vírus Zika e a microcefalia."
 
 #Extrair o texto visivel do html
 def visible_text_from_url(url):
@@ -36,6 +38,32 @@ def ie_preprocess(document):
     #sentences = [nltk.pos_tag(sent) for sent in sentences]
     return sentences
 
+def serialize(graph, destination=None, format="xml",base=None, encoding=None, **args):
+    s = plugin.get(format,Serializer)
+    serializer = s(store=graph)
+    if destination is None:
+        stream = BytesIO()
+        serializer.serialize(stream=stream, base=base, encoding=encoding,**args)
+        return stream.getvalue()
+    if hasattr(destination, "write"):
+        stream = destination
+        serializer.serialize(stream=stream, base=base, encoding=encoding, **args)
+    else:
+        location = destination
+        scheme, netloc, path, params, _query, fragment = urlparse(location)
+        if netloc != "":
+            print("WARNING: not saving as location" +"is not a local file reference")
+            return
+        fd, name = tempfile.mkstemp()
+        stream = os.fdopen(fd, "ab")
+        serializer.serialize(stream=stream, base=base, encoding=encoding, **args)
+        stream.close()
+        if hasattr(shutil, "move"):
+            shutil.move(name, path)
+        else:
+            shutil.copy(name, path)
+            os.remove(name)         
+
 def get_reifications(onto):
     reifications = []
     for i in onto.classes:
@@ -47,28 +75,11 @@ def get_text_sentences(text):
     sentences = nltk.sent_tokenize(' '.join([i for i in text.split()]))
     return [nltk.word_tokenize(sent) for sent in sentences]
 
-def article_annotation(document,concept,authorRef,presentation):
-    AO = Namespace("http://smiy.sourceforge.net/ao/rdf/associationontology.owl")
-    PAV = Namespace("http://cdn.rawgit.com/pav-ontology/pav/2.0/pav.owl")
-    #ANN = Namespace("https://www.w3.org/2000/10/annotation-ns#annotates")
-    AOF = Namespace("http://annotation-ontology.googlecode.com/svn/trunk/annotation-foaf.owl")
-
-    graph = Graph()
-    graph.bind('aof',AOF)
-    graph.bind('ao',AO)
-    #graph.bind('ann',ANN)
-    graph.bind('pav',PAV)
-    lang = 'pt'
-    d = Describer(graph,base = "http://organizacao.com")
-    d.value(RDFS.comment,presentation, lang=lang)
+def article_annotation(d,document,concept,authorRef,presentation,base_file):
     
     #d.rel(RDF.type,ANN.base)#De onde eu tirei isso mds?
     #d.rel(RDF.type,ANN.Annotation)
-    d.rel(RDF.type,AO.Annotation)
-    d.rel(AOF.annotatesDocument,document)
-    d.rel(AO.hasTopic,concept)
-    d.rel(PAV.createdOn,Literal(datetime.datetime.now(),datatype=XSD.date))
-    d.rel(PAV.createdB,authorRef)
+
 
     return d
 
@@ -80,26 +91,50 @@ def gen_article_annotations(text,concept_dict,base,author,description):
     sentences = get_text_sentences(text)
     concepts_found = list()
     search_remaining_concepts = [nltk.word_tokenize(i) for i in concept_dict.keys()]
-    annotations = list()
+    
+    AO = Namespace("http://smiy.sourceforge.net/ao/rdf/associationontology.owl")
+    PAV = Namespace("http://cdn.rawgit.com/pav-ontology/pav/2.0/pav.owl")
+    #ANN = Namespace("https://www.w3.org/2000/10/annotation-ns#annotates")
+    AOF = Namespace("http://annotation-ontology.googlecode.com/svn/trunk/annotation-foaf.owl")
+
+    graph = Graph()
+    try:
+        g.parse("base.rdf", format="xml")
+        d = Describer(graph,base = "http://organizacao.com")
+        d.value(RDFS.comment,presentation, lang=lang)
+    except:
+        print("Arquivo de anotações não encontrado ou ilegível.Outro será criado")
+        d = Describer(graph,base = "http://organizacao.com")
+        
+    graph.bind('aof',AOF)
+    graph.bind('ao',AO)
+    graph.bind('pav',PAV)
+    lang = 'pt'
 
     while len(search_remaining_concepts) > 0:
         biggest_concept_lenght = len(max(search_remaining_concepts,key = len))
         biggest_concept_items = [x for x in search_remaining_concepts if len(x) == biggest_concept_lenght]
         search_remaining_concepts = [x for x in search_remaining_concepts if x not in biggest_concept_items]
-
+            
         for sentence in sentences:
             if len(sentence) >= biggest_concept_lenght:
                 for k in range(0,len(sentence) - biggest_concept_lenght):
                     biggest_concept_items_cp = biggest_concept_items
                     for concept in biggest_concept_items_cp:
                         if concept not in concepts_found:
-                            #Compara o conceito com os itens  de -(k + biggest_concept_lenght) até -k
+                        #Compara o conceito com os itens  de -(k + biggest_concept_lenght) até -k
                             if [i.upper() for i in concept] == [i.upper() for i in sentence[-(k + biggest_concept_lenght):-k]]:
                                 concepts_found.insert(len(concepts_found),concept)
-                                annotations.append(article_annotation(url,concept_dict[' '.join(concept)].uri,author,description).graph)
-                                print('\n')
+                                d.rel(RDF.type,AO.Annotation)
+                                d.rel(AOF.annotatesDocument,url)
+                                d.rel(AO.hasTopic,concept_dict[' '.join(concept)].uri)
+                                d.rel(PAV.createdOn,Literal(datetime.datetime.now(),datatype=XSD.date))
+                                d.rel(PAV.createdB,author)
+                                graph.commit()
+                                d = Describer(graph,base = "http://organizacao.com")
                                 biggest_concept_items.remove(concept)
-    return annotations
+    return graph
+
 
 onto = ontospy.Ontospy("root-ontology.owl")
 web_concepts = get_reifications(onto)
@@ -109,9 +144,6 @@ for i in web_concepts:
     text_concept = str(i.uri).partition('#')[-1].replace('_',' ')
     concept_dict[text_concept] = i
 
-for g in gen_article_annotations(text,concept_dict,"www.semanticdev.com","Vitor Silva","Anotação semântica para textos jornalísticos"):
-    g.commit()
-    g.parse("base.rdf",format='xml')
-    #g.serialize(destination='base.txt')
+serialize(gen_article_annotations(text,concept_dict,"www.semanticdev.com","Vitor Silva","Anotação semântica para textos jornalísticos"),format='xml', destination = 'base.rdf')
 
 
